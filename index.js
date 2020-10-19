5e6ded9851be9c924433f1f7fc7fa7e728e7bd56
Rodrigo Daniel Andrade <rodasistemas@gmail.com>
2020-10-19 19:16:56 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const venom = require('venom-bot');
+
+venom
+  .create()
+  .then((client) => start(client));
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === 'Oi' || message.body === 'Olá') {
+      client
+        .sendText(message.from, 'Olá! Tudo bem com você?')
+        .then((result) => {
+          console.log('Result: ', result); //retorna um objeto de successo
+        })
+        .catch((erro) => {
+          console.error('Erro ao enviar mensagem: ', erro); //return um objeto de erro
+        });
+    }
+  });
+}
